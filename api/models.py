d401ef8be28e73faa5796986910ec9368ba6c540
deepaksaini14896 <deepaksaini14896@gmail.com>
2022-04-28 19:11:17 +0530

    new commit

--- api/models.py
+++ api/models.py
@@ -1,31 +1,35 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Tax_Admin(models.Model):
+	public_id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
 	name = models.CharField(max_length = 50)
 	email = models.EmailField(max_length = 50, unique = True)
 	phone = models.IntegerField(unique = True)
-	pan_number = models.CharField(max_length = 10, primary_key = True)
+	pan_number = models.CharField(max_length = 10, unique = True)
 	password = models.CharField(max_length = 100)
 
 	def __str__(self):
 		return self.name
 
 class Tax_Accountant(models.Model):
+	public_id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
 	name = models.CharField(max_length = 50)
 	email = models.EmailField(max_length = 50, unique = True)
 	phone = models.IntegerField(unique = True)
-	pan_number = models.CharField(max_length = 10, primary_key = True)
+	pan_number = models.CharField(max_length = 10, unique = True)
 	password = models.CharField(max_length = 100)
 
 	def __str__(self):
 		return self.name
 
 class Tax_Payer(models.Model):
+	public_id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
 	name = models.CharField(max_length = 50)
-	gst_number = models.CharField(max_length = 15)
-	pan_number = models.CharField(max_length = 10, primary_key = True)
-	email = models.EmailField(max_length = 50)
+	gst_number = models.CharField(max_length = 15, unique = True)
+	pan_number = models.CharField(max_length = 10, unique = True)
+	email = models.EmailField(max_length = 50, unique = True)
 	phone = models.IntegerField(unique = True)
 	password = models.CharField(max_length = 100)
 
